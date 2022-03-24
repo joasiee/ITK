c84e6f6f9c3c95e7c9576544416f9451a59d75aa
Joas Mulder <joasmulder@hotmail.com>
2022-03-24 02:09:19 +0100

    Revert "default use thread 0"
    This reverts commit 18d13878b35d01e40194e7e2e1244d3c5f958d04.

--- Modules/Core/ImageFunction/include/itkBSplineInterpolateImageFunction.h
+++ Modules/Core/ImageFunction/include/itkBSplineInterpolateImageFunction.h
@@ -161,7 +161,14 @@ public:
   OutputType
   EvaluateAtContinuousIndex(const ContinuousIndexType & index) const override
   {
-    return this->EvaluateAtContinuousIndex(index, 0);
+    // Don't know thread information, make evaluateIndex, weights on the stack.
+    // Slower, but safer.
+    vnl_matrix<long>   evaluateIndex(ImageDimension, (m_SplineOrder + 1));
+    vnl_matrix<double> weights(ImageDimension, (m_SplineOrder + 1));
+
+    // Pass evaluateIndex, weights by reference. They're only good as long
+    // as this method is in scope.
+    return this->EvaluateAtContinuousIndexInternal(index, evaluateIndex, weights);
   }
 
   virtual OutputType
